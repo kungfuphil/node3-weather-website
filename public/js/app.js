31d45bdc9ea73c8c362bfb7eb39fd24d11412e33
Phil Ho <phil@philho.com>
2020-06-30 07:10:37 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -27,7 +27,7 @@ weatherForm.addEventListener('submit', (e) => {
         messageOne.textContent = 'You must enter a value!'
     }
 
-    fetch(`http://localhost:3000/weather?address=${encodeURIComponent(location)}`).then((response) => {
+    fetch(`/weather?address=${encodeURIComponent(location)}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
