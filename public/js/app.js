f08ba0b1b50472cf67d0f0ad3301c456af5db644
Phil Ho <phil@philho.com>
2020-06-29 21:03:44 -0400

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+console.log('Client side javascript file is loaded!')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From Javascript'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const location = search.value
+
+    if (location.length === 0) {
+        messageOne.textContent = 'You must enter a value!'
+    }
+
+    fetch(`http://localhost:3000/weather?address=${encodeURIComponent(location)}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=xxxxx&query=${latitude},${longitude}&units=f`
+
+    console.log(url)
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location!', undefined)
+        } else {
+            weather_description = body.current.weather_descriptions[0]
+            temperature = body.current.temperature
+            feelslike = body.current.feelslike
+
+            callback(undefined, `${weather_description}. It's currently ${temperature} degrees and feels like ${feelslike} degrees.`)
+        }
+    })
+}
+
+module.exports = forecast
